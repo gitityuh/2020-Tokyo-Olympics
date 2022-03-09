71ea5b202688652813ef488a9bc995964efad98a
Quincy Marchelya <marchelya@wisc.edu>
2022-03-09 16:21:34 +0000

    Commit and pushing interface

--- src/IRedBlackTrees.java
+++ src/IRedBlackTrees.java
@@ -1,6 +1,5 @@
 @SuppressWarnings("unchecked")
 public interface IRedBlackTrees<T , K> extends SortedCollectionInterface {
-
   // Note that the provided iterators step through the data within this
   // collection in sorted order, as defined by their compareTo() method.
   public boolean insert(T data, K key) throws NullPointerException, IllegalArgumentException;
