8acb34a9fb43e1ce0b0156daf4bf67396b0e979b
Kevin Lee <ke_vinlee@outlook.com>
2022-03-29 13:12:45 -0500

    Renamed Olympic -> Country, added revised methods and variables for ICountry, changed some comments

---
+++ ICountry.java
@@ -0,0 +1,21 @@
+/**
+ * Instances of classes that implement this interface represents a single
+ * Country object that can be stored, sorted, and searched based on
+ * these accessors below.
+ */
+public interface ICountry extends Comparable<ICountry> {
+
+	// constructor args (String name, int goldMedal, int silverMedal, int bronzeMedal,
+	// int totalMedals, int rank, String continent)
+
+	String getName(); //
+	int getGoldMedals();
+	int getSilverMedals();
+	int getBronzeMedals();
+	int getTotalMedals();
+	int getRank();
+	String getContinent();
+
+	// compareTo() method supports sorting these objects by name and number of total medals
+
+}
