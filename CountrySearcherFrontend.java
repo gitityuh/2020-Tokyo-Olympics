79fef7baa2fc6fc8e230cdf82e828d13e2c2c79e
Kevin Lee <ke_vinlee@outlook.com>
2022-03-30 00:48:28 -0500

    finished implementation of CountrySearcherFrontend

--- CountrySearcherFrontend.java
+++ CountrySearcherFrontend.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -9,6 +10,7 @@ public class CountrySearcherFrontend implements ICountrySearcherFrontend{
 
   Scanner scanner;
   ICountrySearcherBackend backend;
+  String[] continentStrings = {"___", "_x_", "_x_", "_x_", "_x_", "_x_", "_x_"};
 
   /**
    * Reads input from System.in
@@ -42,21 +44,28 @@ public class CountrySearcherFrontend implements ICountrySearcherFrontend{
    * explicitly enters the quit command.
    */
   @Override public void runCommandLoop() {
-    String commandLine = scanner.nextLine().trim();
-    char command = commandLine.charAt(0);
+    System.out.println("Choose a command from the menu above: ");
+    int commandLine = 0;
+    try {
+      commandLine = scanner.nextInt();
+    } catch (InputMismatchException ime) {
+      System.out.println("Input must be a number. Please try again");
+      runCommandLoop();
+    }
+
     // chooses method to call by command
-    switch (command) {
-      case '1':
+    switch (commandLine) {
+      case 1:
         countriesByMedal();
         break;
-      case '2':
-        displayCountries();
+      case 2:
+        displayCountries(backend.outputByAlphabeticalName());
         break;
-      case '3':
+      case 3:
         medalsByCountry();
-      case '4':
+      case 4:
         filterByContinent();
-      case '5':
+      case 5:
         return;
       // if all cases fail, repeat command loop
       default:
@@ -81,6 +90,7 @@ public class CountrySearcherFrontend implements ICountrySearcherFrontend{
         "   4) Filter by Continent\n" +
         "   3) Quit\n"
     );
+    runCommandLoop();
   }
 
   /**
@@ -91,26 +101,124 @@ public class CountrySearcherFrontend implements ICountrySearcherFrontend{
     for (ICountry country : countries) {
       System.out.println(country.getName());
     }
+    displayCommandMenu();
   }
 
   /**
    * reads word from System.in, displays results
    */
   @Override public void countriesByMedal() {
-    ICountry country = backend.searchByName("Canada");
+    System.out.println("Choose a medal type you would like to search for: ");
+    String medalType = "";
+    try {
+      medalType = scanner.nextLine().trim().toLowerCase();
+    } catch (InputMismatchException ime) {
+      System.out.println("Input must be either gold, silver, or bronze. Please try again.");
+      countriesByMedal();
+    }
+    List<ICountry> countries = backend.outputByTypeOfMedals(medalType);
+    int medalCount = 0;
+    for (ICountry country : countries) {
+      switch (medalType) {
+        case "gold":
+          medalCount = country.getGoldMedals();
+        case "silver":
+          medalCount = country.getSilverMedals();
+        case "bronze":
+          medalCount = country.getBronzeMedals();
+        default:
+          System.out.println("Invalid medal type!");
+          countriesByMedal();
+          break;
+      }
+      System.out.println(country.getName() + ": " + medalType + " " + medalCount);
+    }
+    displayCommandMenu();
   }
 
   /**
    * reads year from System.in, displays results
    */
   @Override public void medalsByCountry() {
-
+    System.out.println("Choose a country you would like to search for: ");
+    String countryName = scanner.nextLine().trim().toLowerCase();
+    ICountry countryObject = backend.searchByName(countryName);
+    System.out.println("Gold: " + countryObject.getGoldMedals());
+    System.out.println("Silver: " + countryObject.getSilverMedals());
+    System.out.println("Bronze: " + countryObject.getBronzeMedals());
+    displayCommandMenu();
   }
 
   /**
    * filter toggles by continent
    */
   @Override public void filterByContinent() {
+//    boolean[] continentToggles = {false, true, true, true, true, true, true};
 
+    System.out.println("Continents:\n" +
+        "   1) " + continentStrings[1] + " Africa" +
+        "   2) " + continentStrings[2] + " Asia" +
+        "   3) " + continentStrings[3] + " Oceania" +
+        "   4) " + continentStrings[4] + " Europe" +
+        "   5) " + continentStrings[5] + " North America" +
+        "   6) " + continentStrings[6] + " South America"
+    );
+
+    System.out.println("Choose the continent that you would like to toggle the filter for: ");
+    int toggleNum = 0;
+    try {
+       toggleNum = scanner.nextInt();
+    } catch (InputMismatchException ime) {
+      System.out.println("Filter must be a number. Please try again.");
+      filterByContinent();
+    }
+    switch (toggleNum) {
+      case 1:
+        backend.toggleContinentFilter(1);
+        if (continentStrings[1].equals("_x_")) {
+          continentStrings[1] = "___";
+        } else {
+          continentStrings[1] = "_x_";
+        }
+      case 2:
+        backend.toggleContinentFilter(2);
+        if (continentStrings[2].equals("_x_")) {
+          continentStrings[2] = "___";
+        } else {
+          continentStrings[2] = "_x_";
+        }
+      case 3:
+        backend.toggleContinentFilter(3);
+        if (continentStrings[3].equals("_x_")) {
+          continentStrings[3] = "___";
+        } else {
+          continentStrings[3] = "_x_";
+        }
+      case 4:
+        backend.toggleContinentFilter(4);
+        if (continentStrings[4].equals("_x_")) {
+          continentStrings[4] = "___";
+        } else {
+          continentStrings[4] = "_x_";
+        }
+      case 5:
+        backend.toggleContinentFilter(5);
+        if (continentStrings[5].equals("_x_")) {
+          continentStrings[5] = "___";
+        } else {
+          continentStrings[5] = "_x_";
+        }
+      case 6:
+        backend.toggleContinentFilter(6);
+        if (continentStrings[6].equals("_x_")) {
+          continentStrings[6] = "___";
+        } else {
+          continentStrings[6] = "_x_";
+        }
+      default:
+        System.out.println("Please try again.");
+        filterByContinent();
+    }
+    displayCommandMenu();
   }
 }
--- ICountrySearcherBackend.java
+++ ICountrySearcherBackend.java
@@ -8,17 +8,19 @@ import java.util.List;
 public interface ICountrySearcherBackend {
     public void addCountry(ICountry country); // adds country to backend database
 
-    public int getNumberOfCountries();
-    
+    //public int getNumberOfCountries();
+
     // These methods set the medal filters before calling the search and output methods
     // (all medals are included in the search results by default)
-    public void setMedalType(String medal, boolean filter);
-    public boolean getMedalFilter(String medal);
-    public void toggleMedalFilter(String medal);
+    //public void setMedalType(String medal, boolean filter);
+    //public boolean getMedalFilter(String medal);
+    //public void toggleMedalFilter(String medal);
 
     // These methods return either a list of ICountrys or an ICountry
-    public ICountry searchByName(String name);
-    public List<ICountry> outputByTotalMedals();
-    public List<ICountry> outputByTypeOfMedals(String medalType);
+    ICountry searchByName(String name);
+    //public List<ICountry> outputByTotalMedals();
+    List<ICountry> outputByTypeOfMedals(String medalType);
+    List<ICountry> outputByAlphabeticalName();
+    void toggleContinentFilter(Integer number);
 }
 
