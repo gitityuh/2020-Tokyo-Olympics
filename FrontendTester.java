0b818217f882713aeb00571c65f32db4c5ef1220
Kevin Lee <ke_vinlee@outlook.com>
2022-03-30 19:19:21 -0500

    Finished writing tester methods

--- FrontendTester.java
+++
@@ -1,7 +0,0 @@
-public class FrontendTester {
-  public static void main(String[] args) {
-    CountrySearcherBackend backend = new CountrySearcherBackend();
-    CountrySearcherFrontend frontend = new CountrySearcherFrontend(backend);
-
-  }
-}
