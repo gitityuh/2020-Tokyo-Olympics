4c05f237b9483a61052c83443a373f69348ae2a5
Kevin Lee <ke_vinlee@outlook.com>
2022-04-03 20:32:48 -0500

    another cleanup

--- OlympicCountryApp.java
+++ OlympicCountryApp.java
@@ -5,11 +5,11 @@ public class OlympicCountryApp {
 
 
         public static void main(String[] args) throws Exception {
-            ICountryLoader loader = new CountryLoaderBackEnd(); //new ShowLoader();
+            ICountryLoader loader = new CountryLoaderBackend(); //new ShowLoader();
             List<ICountry> countries = loader.loadData("Tokyo_Medals_2021.xml");
             ICountrySearcherBackend backend = new OlympicsBackend(); //new ShowSearcherBackend();
             for(ICountry country : countries) backend.addCountry(country);
-            ICountrySearcherFrontend frontend = new CountrySearcherFrontendBackEnd(); //new ShowSearcherFrontend(backend);
+            ICountrySearcherFrontend frontend = new CountrySearcherFrontendBackend(); //new ShowSearcherFrontend(backend);
             frontend.runCommandLoop();
         }
 
