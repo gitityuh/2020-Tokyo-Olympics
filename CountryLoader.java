9a88d8801d4101546a66290f4264a13d51f6f111
George Gervasio <ggervasio@wisc.edu>
2022-04-06 21:39:22 +0000

    reformated my countryLoader class

--- CountryLoader.java
+++ CountryLoader.java
@@ -13,7 +13,12 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-
+/*
+ *This class loads the countries from the xml file into the
+ * country class
+ *
+ *
+*/
 public class CountryLoader implements ICountryLoader{
     String countryName;
     int gold;
@@ -21,55 +26,49 @@ public class CountryLoader implements ICountryLoader{
     int bronze;
     int rank;
     String continent;
-
+    
+    
+    /*
+    This method loads the data from the olympic xml file and imports
+    it to the Country class
+     */
     @Override
     public List<ICountry> loadData(String filepath) throws FileNotFoundException {
         List<ICountry> retList = new ArrayList<>();
+        //loads xml to a file
         File xml = new File(filepath);
         if(!xml.exists()){
             throw new FileNotFoundException("filepath is invalid");
         }
+        //This is thexml file parser I got from a TA piazza link
+        
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-try{
-    DocumentBuilder db = dbf.newDocumentBuilder();
-    Document doc = db.parse(xml);
-    NodeList list = doc.getElementsByTagName("row");
+        try{
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(xml);
+            NodeList list = doc.getElementsByTagName("row");
+            for(int y = 0; y<list.getLength();y++){
+                Node node = list.item(y);
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element element = (Element) node;
+                    countryName = element.getElementsByTagName("Country").item(0).getTextContent();
+                    
+                    gold = Integer.parseInt(element.getElementsByTagName("Gold_Medal").item(0).getTextContent());
+                    silver = Integer.parseInt(element.getElementsByTagName("Silver_Medal").item(0).getTextContent());
+                    bronze = Integer.parseInt(element.getElementsByTagName("Bronze_Medal").item(0).getTextContent());
+                    rank = Integer.parseInt(element.getElementsByTagName("Rank_By_Total").item(0).getTextContent());
+                    continent = element.getElementsByTagName("Continent").item(0).getTextContent();
+                    Country newCountry = new Country(countryName,gold,silver,bronze,rank,continent);
+                    retList.add(newCountry);
+                }
+            }
 
-    for(int y = 0; y<list.getLength();y++){
-        Node node = list.item(y);
-        if (node.getNodeType() == Node.ELEMENT_NODE) {
-            Element element = (Element) node;
-
-            countryName = element.getElementsByTagName("Country").item(0).getTextContent();
-            //System.out.println(countryName);
-            gold = Integer.parseInt(element.getElementsByTagName("Gold_Medal").item(0).getTextContent());
-            silver = Integer.parseInt(element.getElementsByTagName("Silver_Medal").item(0).getTextContent());
-            bronze = Integer.parseInt(element.getElementsByTagName("Bronze_Medal").item(0).getTextContent());
-            rank = Integer.parseInt(element.getElementsByTagName("Rank_By_Total").item(0).getTextContent());
-            continent = element.getElementsByTagName("Continent").item(0).getTextContent();
-
-            Country newCountry = new Country(countryName,gold,silver,bronze,rank,continent);
-            retList.add(newCountry);
-
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
         }
-    }
-
-
-
-
-
-
-
-
-
-}catch (FileNotFoundException e){
-    e.printStackTrace();
-}
-
-
-catch (ParserConfigurationException  | SAXException | IOException  e) {
-    e.printStackTrace();
-}
+        catch (ParserConfigurationException  | SAXException | IOException  e) {
+            e.printStackTrace();
+        }
         return retList;
     }
 }
